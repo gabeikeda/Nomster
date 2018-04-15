c97dffd9481f2c37b5f96be4b49fdb5f8ad807f6
Gabriel <gabeikeda@gmail.com>
2018-04-15 23:55:17 +0000

    Email Basics

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,15 +1,8 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
+  default from: "ikedaemailtest@gmail.com"
 
-  def comment_added(comment)
-    @place = comment.place
-    @place_owner = @place.user
-    mail(to: @place_owner.email,
-         subject: "A comment has been added to #{@place.name}")
-  end
-
-  def test
-    mail(to: ENV['GMAIL_ADDRESS'],
+  def comment_added
+    mail(to: "ikedaemailtest@gmail.com",
          subject: "A comment has been added to your place")
   end
 end
