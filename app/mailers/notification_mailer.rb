3de5868be4c352b332099336452126b1a01826e0
Gabriel <gabeikeda3@gmail.com>
2018-04-17 22:39:37 +0000

    fixed email send on comment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
-  default from: "ikedaemailtest@gmail.com"
+  default from: "ikedaemailtester@gmail.com"
 
-  def comment_added
-    mail(to: "ikedaemailtest@gmail.com",
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
          subject: "A comment has been added to your place")
   end
 end
